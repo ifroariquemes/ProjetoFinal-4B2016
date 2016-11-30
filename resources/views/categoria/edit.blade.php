e3a6caefb07b6d3136fab6e916331a299c008118
03446525211 <03446525211@LA03.ifro.local>
2016-11-30 16:10:29 -0400

    Views categoria.

---
+++ resources/views/categoria/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-sm-8 blog-main">
+
+            <div class="blog-post">
+                <h2 class="blog-post-title">Editar categoria</h2>
+                <hr>
+                <form action="{{ URL::to('/categoria/update') }}" method="post">
+                    <div class="form-group">
+                        <div class="form-group">
+                            <label for="titulo">Categoria</label>
+                            <input type="text" class="form-control" name="categoria" id="titulo">
+                        </div>
+                        <button type="submit" class="btn btn-success">Salvar <i class="glyphicon glyphicon-ok"></i></button>
+                </form>
+            </div>
+        </div><!-- /.blog-post -->
+        <hr>
+    </div><!-- /.blog-main -->
+
+</div><!-- /.row -->
+
+</div><!-- /.container -->
+@endsection
