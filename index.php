6a920f1a3c7bf20cd908bcbd27076593460dc502
pepem <pepem@DESKTOP-580M00E>
2016-12-01 21:35:16 -0200

    Etapa 3 realizada, pronta para etapa 4;

--- index.php
+++ index.php
@@ -8,38 +8,44 @@
     </head>
     <body>
         <section id="wrap-geral">
-        <ul id="prova-lista">
-            <?php
+            <ul id="prova-lista">
+                <?php
                 $prova = $pdo->prepare("SELECT * FROM `provas` WHERE `status` = 1 ORDER BY `id_prova` DESC");
                 $provas->execute();
-                
+
                 while ($row = $provas->fetchObject()) {
-            ?>
-            <li class="open-prova" data-id="<?php echo $row->id;?>">
-                    <a href="prova.php?prova=<?php echo $row->id_prova; ?>">
-                        <?php echo $row->titulo; ?>
-                    </a>
-                </li>
-            <?php } ?>
-        </ul>
+                    ?>
+                    <li class="open-prova" data-id="<?php echo $row->id; ?>">
+                        <a href="prova.php?prova=<?php echo $row->id_prova; ?>">
+                            <?php echo $row->titulo; ?>
+                        </a>
+                    </li>
+                <?php } ?>
+            </ul>
             <section id="wrap-prova">
-            <h1><PROVA</h1>
-            
-            <div class="questoes">
+                <div class='begin'>
+                    <button id='comecar' class='button azul'>Iniciar Prova</button>
+                </div>
+                <h1><PROVA</h1>
 
-            </div>
-            
-            
+                <div class="questoes">
 
-            <button class="button" id="prev">Anterior</button>
-            <button class="button" id="next">Proxima</button>
-        </section>
+                </div>
+
+
+
+                <button class="button" id="prev">Anterior</button>
+                <button class="button" id="next">Proxima</button>
+            </section>
             <div style="clear:both;"></div>  
         </section>
 
+        <div style="clear:both;"></div>
+        <span class="timer">39:59</span>
+    </section>
         <script type="text/javascript" src="js/jquery.js"></script>
         <script type="text/javascript" src="js/function.js"></script>
-        
+
     </body>
 </html>
 
--- js/function.js
+++ js/function.js
@@ -1,37 +1,72 @@
-$(function(){
+/* global e */
+
+$(function () {
     var provaId = 0;
-   $('.open-prova').on('click', function(e){
-      e.preventDefault();
-      provaId = $(this).attr('data-id');
-      $('.questoes').html('');
-      
-      $.ajax({
-          method:'POST',
-          url: 'sys/getProva.php',
-          dataType: 'json',
-          data: {prova: provaId},
-          success: function(r){
-              $('#wrape-prova h1').html(r.titulo);
-              //console.log(r.questoes);
-              $.each(r.questoes, function(){
-                 var questao = this;
-                 var html = '<div class="questao">';
-                     html +='<h2>'+questao.questao+'</h2>';
-                     if(questao.tipo == 0){
-                        $.each(questao.opcoes, function(){
-                              var opcao = this;
-                              html+= '<p><input type="radio" class="resposta" name="resposta'+questao.id+'" value="'+opcao.id+'"/>'+opcao.opcao+'</p>';
-                         });
-                     }else{
-                         html+='<input type="text" name="resposta'+questao.id+'" />';
-                     }
-                     html+= '</div>';
-                $('.questoes').append(html);
-              });
-          }
-      });
-      return false;
-   });
+    $('.open-prova').on('click', function (e) {
+        e.preventDefault();
+        $('#next, $#prev', '.begin').show();
+        provaId = $(this).attr('data-id');
+        $('.questoes').html('');
+
+        $.ajax({
+            method: 'POST',
+            url: 'sys/getProva.php',
+            dataType: 'json',
+            data: {prova: provaId},
+            success: function (r) {
+                $('#wrape-prova h1').html(r.titulo);
+                //console.log(r.questoes);
+                $.each(r.questoes, function () {
+                    var questao = this;
+                    var html = '<div class="questao">';
+                    html += '<h2>' + questao.questao + '</h2>';
+                    if (questao.tipo == 0) {
+                        $.each(questao.opcoes, function () {
+                            var opcao = this;
+                            html += '<p><input type="radio" class="resposta" name="resposta' + questao.id + '" value="' + opcao.id + '"/>' + opcao.opcao + '</p>';
+                        });
+                    } else {
+                        html += '<input type="text" name="resposta' + questao.id + '" />';
+                    }
+                    html += '</div>';
+                    $('.questoes').append(html);
+                });
+            }
+        });
+        return false;
+    });
+
+    var iterator = 0;
+
+    $('body').on('click', '#next', function (e) {
+        e.preventDefault();
+        iterator++;
+        var qtdQuestoes = ($('.questao').lenght-1);
+        If (iterator === qtdQuestoes){
+            $(this).attr('id', 'concluir');
+            $(this).addClass('azul');
+            $(this).text('concluir');
+        };
+        $('.questao').hide();
+        $('.questao:eq(' + iterator + ')').show();
+        return false;
+    });
+    $('body').on('click', '#prev', function(e)){
+        e.preventDefault();
+        var qtdQuestoes = ($('.questao').lenght-1);
+        if (iterator === qtdQuestoes){
+            $('#concluir').removeClass('azul');
+            $('#concluir').text('Proxima');
+            $('#concluir').attr('id', '#next');
+        }
+        iterator--;
+        if(iterator < 0){
+            iterator = 0;   
+        }
+        $('.questao').hide();
+        $('.questao:eq('+iterator+')').show();
+        return false;
+    }
 });
 
 
