80f1d35e9012071ab4c9e0096a851ca20a24804e
alessandra18rodrigues <alessandramoreirarodrigues@gmail.com>
2016-12-05 22:23:34 -0300

    Parte do login

---
+++ application/controllers/usuario_controll.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class Usuario_controll extends CI_Controller{
+    
+	public function index()
+	{
+		$this->load->view('login_view');
+           
+	}
+}
---
+++ application/views/login_view.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+  <div class="login-page">
+  <div class="form">
+    <form class="register-form">
+      <input type="text" placeholder="Login"/>
+      <input type="password" placeholder="Senha"/>
+      <!-- <input type="text" placeholder="email address"/> -->
+      <button>Criar</button>
+      <p class="message">Já possui conta? <a href="#">Entrar</a></p>
+    </form>
+    <form class="login-form">
+      <input type="text" placeholder="Login"/>
+      <input type="password" placeholder="Senha"/>
+      <button>Entrar</button>
+      <p class="message">Não possui conta? <a href="#">Criar conta</a></p>
+    </form>
+  </div>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+</body>
+</html>
+
+
